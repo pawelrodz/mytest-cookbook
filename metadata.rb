f0015e24dfee44cf7fa732ece232bc9f11c8b091
unknown <212356769@MYPLLWGDA500981.R2.Money.ge.com>
2014-11-27 15:09:44 +0100

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'mytest-cookbook'
+maintainer       'mnv'
+maintainer_email 'nvnv@wp.pl'
+license          'All rights reserved'
+description      'Installs/Configures mytest-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
